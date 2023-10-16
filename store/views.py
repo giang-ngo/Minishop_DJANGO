fded3401d8729120cc6baf47a6de938f18697198
Your <ngothuygiang.io@gmail.com>
2023-10-16 14:06:08 +0700

    Products by category

--- store/views.py
+++ store/views.py
@@ -1,17 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from store.models import Product
 from django.db.models import Q
 from django.core.paginator import Paginator
+from category.models import Category
 
 
+def store(request, category_slug=None):
+    categories = None
+    products = None
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(
+            category=categories, is_available=True)
+        page = Paginator(products, 6)
+        page_list = request.GET.get('page')
+        page = page.get_page(page_list)
+        product_count = products.count()
 
-def store(request):
-    products = Product.objects.all().filter(is_available=True)
-    page = Paginator(products, 1)
-    page_list = request.GET.get('page')
-    page = page.get_page(page_list)
-    context = {'products': products, 'page': page}
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        page = Paginator(products, 6)
+        page_list = request.GET.get('page')
+        page = page.get_page(page_list)
+        product_count = products.count()
+    context = {'products': products, 'page': page,
+               'product_count': product_count}
     return render(request, 'store/store.html', context)
+
 
 def search(request):
     if 'keyword' in request.GET:
