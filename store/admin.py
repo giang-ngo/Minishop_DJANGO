7e03855f566894ec4c16efbf086df24c1c18ba71
Your <ngothuygiang.io@gmail.com>
2023-10-27 23:09:33 +0700

    ReviewRating model

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Variation
+from .models import Product, Variation, ReviewRating
 # Register your models here.
 
 
@@ -13,8 +13,15 @@ class VariationAdmin(admin.ModelAdmin):
     list_display = ('product', 'variation_category',
                     'variation_value', 'is_active')
     list_editable = ('is_active',)
-    list_filter=('product','variation_category','variation_value',)
+    list_filter = ('product', 'variation_category', 'variation_value',)
+
+
+class ReviewRatingAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user',
+                    'review', 'created_at',)
+    list_filter = ('user',)
 
 
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Variation,VariationAdmin)
+admin.site.register(Variation, VariationAdmin)
+admin.site.register(ReviewRating, ReviewRatingAdmin)
